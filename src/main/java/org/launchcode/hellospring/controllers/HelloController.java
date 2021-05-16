4d8e8f72cb1ffb34a72b920b2a3b624e410eefed
Ryan <ryan.mossor@gmail.com>
2021-05-16 15:15:10 -0500

    Completed chapter 10 exercises and bonus

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -33,13 +33,35 @@ public class HelloController {
         return "Hello, " + name + "!";
     }
 
+    @RequestMapping(value="{langGreeting}", method={RequestMethod.GET, RequestMethod.POST})
+    public static String createMessage(@RequestParam String name, @RequestParam String language) {
+        if (language.equals("spanish")) {
+            return "<h1 style='color:green;font-family:Arial;'>Hola, " + name + "!</h1>";
+        } else if (language.equals("french")) {
+            return "<h1 style='color:blue;font-family:Arial;'>Hola, " + name + "!</h1>";
+        } else if (language.equals("german")) {
+            return "<h1 style='color:red;font-family:Arial;'>Hola, " + name + "!</h1>";
+        } else if (language.equals("swedish")) {
+            return "<h1 style='color:deepskyblue;font-family:Arial;'>Hola, " + name + "!</h1>";
+        } else {
+            return "<h1 style='color:purple;font-family:Arial;'>Hola, " + name + "!</h1>";
+        }
+    }
+
     // lives at /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                     "<body>" +
-                        "<form action='hello' method='POST'>" + // submit a request to /hello
+                        "<form action='langGreeting' method='POST'>" + // submit a request to /hello
                             "<input type='text' name='name'/>" +
+                            "<select name='language'>" +
+                                "<option value='english'>English</option>" +
+                                "<option value='spanish'>Spanish</option>" +
+                                "<option value='french'>French</option>" +
+                                "<option value='german'>German</option>" +
+                                "<option value='swedish'>Swedish</option>" +
+                            "</select>" +
                             "<input type='submit' value='Greet me!'/>" +
                         "</form>" +
                     "</body>" +
